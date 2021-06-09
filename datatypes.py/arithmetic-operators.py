832d85df90ca1f870526b45f3d9e33324b6b2eb9
muthoni12 <muthoningarachu02@gmail.com>
2021-06-09 05:07:36 -0700

    initial commit

--- datatypes.py/arithmetic-operators.py
+++ datatypes.py/arithmetic-operators.py
@@ -5,18 +5,43 @@ calculate the VAT amount then return the inclusive price.
 
 from typing import AsyncGenerator
 
+exclusivePrice = 50
+vatTax = 0.16 #16/100
+vatAmount = 0
+inclusivePrice = 0
 
-def calculateVAT(exclusivePrice, vatTax):
+vatAmount = exclusivePrice * vatTax
+inclusivePrice = exclusivePrice + vatAmount
+
+print(exclusivePrice)
+
+Given an inclusive price and VAT Tax provided by the user,
+calculate the VAT amount then return the exclusive price.
+
+inclusivePrice = 53
+vatTax = 0.17 #17/100
+vatAmount = 0
+exclusivePrice = 0
+
+vatAmount = inclusivePrice * vatTax
+exclusivePrice = inclusivePrice - vatAmount
+
+print(exclusivePrice)
+
+
+def calculateVAT(inclusivePrice, vatTax):
     vatAmount = 0
-    inclusivePrice = 0
+    exclusivePrice = 0
 
     vatAmount = exclusivePrice * vatTax
     inclusivePrice = exclusivePrice + vatAmount
 
     return inclusivePrice
 
-def userInput(age, name):
+def userInput():
+    inclusivePrice = float(input("inclusive price"))
+    vatTax = float(input("vatTax"))
 
-    calculateVAT(exclusivePrice, vatTax):
+    calculateVAT(inclusivePrice, vatTax):
 
-userInput(age, name):
+userInput():
